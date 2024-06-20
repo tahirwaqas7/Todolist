7d0bebbc51f6e5356493693407ee16484930de85
TAHIR WAQAS <147711205+tahirwaqas7@users.noreply.github.com>
2024-06-20 13:23:26 +0500

    Todolist

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <nav className='flex justify-between bg-violet-800 text-white py-3'>
+            <div className="logo">
+                <span className='font-bold text-xl mx-8'>iTask</span>
+            </div>
+            <ul className="flex gap-9 mx-8">
+                <li className='cursor-pointer hover:font-bold transition-all duration-100'>Home</li>
+                <li className='cursor-pointer hover:font-bold transition-all duration-100'>Your Tasks</li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Navbar
