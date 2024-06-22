68a8b47eda7a007e9b5d38dbc6723e84bc3f7892
TAHIR WAQAS <147711205+tahirwaqas7@users.noreply.github.com>
2024-06-22 09:51:16 +0500

    Todolist

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,19 @@ import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import Navbar from './components/Navbar';
 import { v4 as uuidv4 } from 'uuid';
+import { LuClipboardEdit } from "react-icons/lu";
+import { MdDeleteSweep } from "react-icons/md";
+
+import './index.css'; // Import the global styles
+// import App from './App';
+
+
 
 function App() {
 
   const [todo, settodo] = useState("");
   const [todos, settodos] = useState([]);
+  const [showFinished, setshowFinished] = useState(false);
 
   useEffect(() => {
     let todostring = localStorage.getItem("todos");
@@ -36,6 +44,10 @@ function App() {
     });
     settodos(newTodos);
     saveToLS(newTodos);
+  }
+
+  const toggleFinished = (e) => {
+    setshowFinished(!showFinished)
   }
 
   const handleAdd = () => {
@@ -71,35 +83,52 @@ function App() {
 
   return (
     <>
-      <Navbar />
-      <div className="container mx-auto my-5 rounded-xl p-5 bg-violet-100 min-h-[80vh]">
-        <div className="addTodo my-5">
+      <div className="md:container my-5 rounded-xl p-5 bg-violet-100 min-h-[80vh] md:w-[40%] md:mx-auto mx-5">
+        <h1 className='font-semibold capitalize text-center text-xl'>iTask - manage your Todos at one place</h1>
+        <div className="addTodo my-5 flex flex-col gap-4">
+
           <h2 className='text-lg font-bold'>Add a Todo</h2>
-          <input type="text" className='w-1/2' onChange={handleChange} value={todo} />
-          <button onClick={handleAdd} className='bg-violet-800 hover:bg-violet-950 p-2 py-1 text-sm font-bold text-white rounded-md mx-6'>Save</button>
+
+          <div className="flex gap-2">
+            <input type="text" className='w-full outline-none rounded-md p-1 px-3' onChange={handleChange} value={todo} />
+
+            <button onClick={handleAdd} disabled={todo.length <= 0} className='bg-violet-800 hover:bg-violet-950 p-4 py-2 mx-2 disabled:bg-violet-600  text-sm font-bold text-white rounded-md '>Save</button>
+          </div>
+
         </div>
 
-        <h2 className='text-lg font-bold'>Your Todos</h2>
+        <input type="checkbox" onChange={toggleFinished} checked={showFinished} />
+        <label className='mx-2' htmlFor="show">Show Finished</label>
+        <div className='h-[1px] bg-purple-600 opacity-15 w-[80%] mx-auto my-3'></div>
+
+        <h2 className='text-lg font-bold flex justify-center'>Your Todos</h2>
+
 
         <div className="todos">
           {todos.length === 0 && <div className='m-5'>No Todos to display</div>}
+
           {todos.map(item => {
             return (
-              <div key={item.id} className="todo flex w-1/4 my-3 justify-between">
+              (showFinished || !item.isCompleted) && <div key={item.id} className="todo flex md:w-full my-3 justify-between  p-3 h-full bg-[#fff] rounded-lg bg-clip-padding backdrop-filter backdrop-blur-2xl bg-opacity-60 border border-slate-300 gap-x-1">
                 <div className='flex gap-5'>
                   <input name={item.id} onChange={handleCheckbox} type="checkbox" checked={item.isCompleted} />
                   <div className={item.isCompleted ? "line-through" : ""}>{item.todo}</div>
                 </div>
                 <div className="buttons flex h-full">
 
-                  <button onClick={(e) => { handleEdit(e, item.id) }} className='bg-violet-800 hover:bg-violet-950 p-2 py-1 text-sm font-bold text-white rounded-md mx-1'>Edit</button>
+                  <button onClick={(e) => { handleEdit(e, item.id) }} className='bg-violet-800 hover:bg-violet-950 p-2 py-1 font-bold text-white rounded-md mx-1 text-lg '><LuClipboardEdit />
 
-                  <button onClick={(e) => { handleDelete(e, item.id) }} className='bg-violet-800 hover:bg-violet-950 p-2 py-1 text-sm font-bold text-white rounded-md mx-1'>Delete</button>
+                  </button>
+
+                  <button onClick={(e) => { handleDelete(e, item.id) }} className='bg-violet-800 hover:bg-violet-950 p-2 py-1 text-lg font-bold text-white rounded-md mx-1'><MdDeleteSweep />
+
+                  </button>
 
                 </div>
               </div>
             );
           })}
+
         </div>
       </div>
     </>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Navbar = () => {
     return (
-        <nav className='flex justify-between bg-violet-900 text-white py-3'>
+        <nav className='flex justify-around bg-violet-900 text-white py-3'>
             <div className="logo">
                 <span className='font-bold text-xl mx-8'>iTask</span>
             </div>
